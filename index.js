eaa069defedc5a3c27c93d1ab2628b2bb96e77c7
masum khan <study.contents.of.masumkhan@gmail.com>
2023-10-05 02:00:59 +0600

    starter pack for mern based backend

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const initDB = require("./src/datatier/mongodb");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require('cors')
+
+// initialize the database
+initDB();
+
+// middlewares
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors({credentials:true,origin:'http://localhost:5173'})); 
+// 
+
+// const whitelist = ['http://localhost:5173', 'http://blablabla.com'];
+// const corsOptions = {
+//   credentials: true, // This is important.
+//   origin: (origin, callback) => {
+//     if(whitelist.includes(origin))
+//       return callback(null, true)
+//       callback(new Error('Not allowed by CORS'));
+//   }
+// }
+// app.use(cors(corsOptions));
+
+
+// routes
+app.use("/auth", require("./src/routes/auth"));
+
+app.listen(3000, () => {
+  console.log("running ...");
+});
+
+// // close the server
+app.get("/quit", function (req, res) {
+  res.send("closed");
+});
+// server closing endpoint; no need what so ever
+app.get("/", (req, res) => {
+  res.send(`<a href="/quit">quit</a>`);
+});
